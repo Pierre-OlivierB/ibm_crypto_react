d205125acd4f1d82f176d2f5fe569ef52f535606
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2023-08-02 10:47:13 +0200

    add treemap chart with tooltip on hover

--- src/components/GlobalChart.jsx
+++ src/components/GlobalChart.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Tooltip, Treemap } from "recharts";
 
 function GlobalChart({ coinsData }) {
-  console.log(coinsData);
+  const [dataArray, setDataArray] = useState([]);
+
+  useEffect(() => {
+    let chartData = [];
+    if (coinsData.length > 0) {
+      for (let i = 0; i < 45; i++) {
+        chartData.push({
+          name:
+            coinsData[i].symbol.toUpperCase() +
+            " " +
+            coinsData[i].market_cap_change_percentage_24h.toFixed(1) +
+            "%",
+          size: coinsData[i].market_cap,
+          fill: null,
+        });
+      }
+    }
+    setDataArray(chartData);
+  }, [coinsData]);
+
+  const TreemapToolTip = ({ active, payload }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label">{payload[0].payload.name}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="global-chart">
-      <h1>test g chart</h1>
+      <Treemap
+        width={730}
+        height={181}
+        data={dataArray}
+        dataKey="size"
+        stroke="rgb(51,51,51)"
+        fill="black"
+        aspectRatio="1"
+      >
+        <Tooltip content={<TreemapToolTip />} />
+      </Treemap>
     </div>
   );
 }
