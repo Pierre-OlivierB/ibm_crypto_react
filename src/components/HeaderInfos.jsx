22c49c021102643480bdcb6993a5afa3b12452e6
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2023-08-02 09:18:59 +0200

    header content with data from readme first link

--- src/components/HeaderInfos.jsx
+++ src/components/HeaderInfos.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PercentChange from "./PercentChange";
 
 function HeaderInfos() {
+const [headerData,setHeaderData]=useState([])
+
+useEffect(()=>{
+axios.get("https://api.coingecko.com/api/v3/global").then((res)=>setHeaderData(res.data.data))
+},[])
+
+
   return (
     <div className="header-container">
-      <h1>header info test</h1>
+      <ul className="title">
+        <li>
+          <h1><img src="./assets/logo.png" alt="logo crypto"/>Watch Tower</h1>
+        </li>
+        <li>
+          Crypto-Monnaies : {headerData.active_cryptocurrencies && headerData.active_cryptocurrencies.toLocaleString()}
+        </li>
+        <li>
+          Marchés : {headerData.markets && headerData.markets}
+        </li>
+      </ul>
+      <ul className="info-mkt">
+        <li className="global-mkt">
+          Global Market Cap : <PercentChange percent={headerData.market_cap_change_percentage_24h_usd} />
+        </li>
+      </ul>
     </div>
   );
 }
