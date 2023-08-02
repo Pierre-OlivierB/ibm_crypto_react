41d33117ec0c4c551a3097e4b239951f7bb87774
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2023-08-02 10:19:30 +0200

    add axios to app for datas all app and add global chart comp in header

---
+++ src/components/GlobalChart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GlobalChart(props) {
+  return (
+    <div className="global-chart">
+      <h1>test g chart</h1>
+    </div>
+  );
+}
+
+export default GlobalChart;
