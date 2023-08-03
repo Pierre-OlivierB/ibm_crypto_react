a4dcb22959ad7271d0f04b52d2cf0b326c3e86a1
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2023-08-03 09:45:24 +0200

    add chart component on over each money

---
+++ src/components/CoinChart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CoinChart(props) {
+  return (
+    <div className="coin-chart">
+      <h1>Coin Chart</h1>
+    </div>
+  );
+}
+
+export default CoinChart;
--- src/components/TableLine.jsx
+++ src/components/TableLine.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import CoinChart from "./CoinChart";
 import PercentChange from "./PercentChange";
 import StarIcon from "./StarIcon";
 
 function TableLine({ coin, index }) {
+  const [showChart, setShowChart] = useState(false);
+
   const priceFormater = (num) => {
     if (Math.round(num).toString().length < 4) {
       return new Intl.NumberFormat("us-US", {
@@ -27,8 +30,15 @@ function TableLine({ coin, index }) {
           <img src={coin.image} height="20" alt="logo coin" />
         </div>
         <div className="infos">
-          <div className="chart-img">
+          <div
+            className="chart-img"
+            onMouseEnter={() => setShowChart(true)}
+            onMouseLeave={() => setShowChart(false)}
+          >
             <img src="./assets/chart-icon.svg" alt="chart-icon" />
+            <div className="chart-container" id={coin.name}>
+              {showChart && <CoinChart />}
+            </div>
           </div>
           <h4>{coin.name}</h4>
           <span>- {coin.symbol.toUpperCase()}</span>
