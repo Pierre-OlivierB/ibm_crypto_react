f1229e88d37e3d8953b6b8587618e1691433cf03
Pierre-Olivier BAUDINO <baudinopierreolivier@gmail.com>
2023-08-03 08:02:23 +0200

    add header table for data map

---
+++ src/components/Table.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+
+function Table({ coinsData }) {
+  const [rangeNumber, setRangeNumber] = useState(100);
+  const [orderBy, setOrderBy] = useState("");
+
+  const tableHeader = [
+    "Prix",
+    "MarketCap",
+    "Volume",
+    "1h",
+    "1j",
+    "1m",
+    "6m",
+    "1a",
+    "ATH",
+  ];
+
+  return (
+    <div className="table-container">
+      <ul className="table-header">
+        <div className="range-container">
+          <span>
+            Top{" "}
+            <input
+              type="text"
+              value={rangeNumber}
+              onChange={(e) => setRangeNumber(e.target.value)}
+            />
+          </span>
+          <input
+            type="range"
+            name=""
+            id=""
+            min="1"
+            max="250"
+            value={rangeNumber}
+            onChange={(e) => setRangeNumber(e.target.value)}
+          />
+        </div>
+        {tableHeader.map((el) => (
+          <li key={el}>
+            <input
+              type="radio"
+              name="header"
+              id={el}
+              defaultChecked={
+                el === orderBy || el === orderBy + "reverse" ? true : false
+              }
+              onClick={() => {
+                if (orderBy === el) {
+                  setOrderBy(el + "reverse");
+                } else {
+                  setOrderBy(el);
+                }
+              }}
+            />
+            <label htmlFor={el}>{el}</label>
+          </li>
+        ))}
+      </ul>
+      {coinsData &&
+        coinsData.slice(0, rangeNumber).map((coin) => <h1>{coin.symbol}</h1>)}
+    </div>
+  );
+}
+
+export default Table;
